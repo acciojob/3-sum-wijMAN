d3f85de0afd2a3060bb2d76d1d29c44e26c5e492
wijman <wish6242@gmail.com>
2023-01-18 15:24:13 +0530

    4th commit

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
-let distance = Infinity;
-let value = Infinity;
-
 function threeSum(arr, target) {
-  //your code here
-  // arr.sort
+  //applying binary search  algo after sorting
+  "use strict";
+  let currentSum = Infinity;
+  let currentDistance = Infinity - target;
+  arr.sort((x, y) => {
+    return x - y;
+  });
+
   for (let i = 0; i < arr.length - 2; i++) {
-    for (let j = i + 1; j < arr.length - 1; j++) {
-      for (let k = j + 1; k < arr.length; k++) {
-        fun2(i, j, k, target, arr);
+    let l = i + 1,
+      r = arr.length - 1;
+
+    while (l < r) {
+      let calculatedSum = arr[i] + arr[l] + arr[r];
+      let calculatedDistance = Math.abs(calculatedSum - target);
+      if (calculatedDistance < currentDistance) {
+        currentDistance = calculatedDistance;
+        currentSum = calculatedSum;
       }
+      //now keep the serch of smaller distance continue
+      if (currentSum > target) r--;
+      else l++;
     }
   }
-  return value;
+
+  return currentSum;
 }
-
-let fun2 = (i, j, k, target, arr) => {
-  let sum = arr[i] + arr[j] + arr[k];
-  let dis = -1;
-  if (sum > target) dis = sum - target;
-  else dis = target - sum;
-
-  if (dis <= distance) {
-    if (value > sum) value = sum;
-    distance = dis;
-  }
-};
-
-// let a = [-1, 2, 1, 4];
-// console.log(threeSum(a, 1));
 
 module.exports = threeSum;
