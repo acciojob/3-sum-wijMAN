cf843c1f715fcebecb5015041b841a67e48d30ba
wijman <wish6242@gmail.com>
2023-01-18 18:01:08 +0530

    12th commit

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function threeSum(arr, target) {
 
     while (l < r) {
       let calculatedSum = arr[i] + arr[l] + arr[r];
+      let calculatedDistance = Math.abs(calculatedSum - target);
       if (calculatedDistance < currentDistance) {
         currentDistance = calculatedDistance;
         currentSum = calculatedSum;
