cf79871baa63fa292507624f1f4a94ca9472a812
wijman <wish6242@gmail.com>
2023-01-18 17:58:10 +0530

    11th commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function threeSum(arr, target) {
   //applying binary search  algo after sorting
   "use strict";
   let currentSum = Infinity;
-  // let currentDistance = Infinity - target;
+  let currentDistance = Infinity - target;
   arr.sort((x, y) => {
     return x - y;
   });
@@ -13,24 +13,20 @@ function threeSum(arr, target) {
 
     while (l < r) {
       let calculatedSum = arr[i] + arr[l] + arr[r];
-      // let calculatedDistance = Math.abs(calculatedSum - target);
-      // let currentDistance = Math.abs(currentSum - target);
-      if (Math.abs(calculatedSum - target) < Math.abs(currentSum - target)) {
-        // currentDistance = calculatedDistance;
+      if (calculatedDistance < currentDistance) {
+        currentDistance = calculatedDistance;
         currentSum = calculatedSum;
       }
       //now keep the serch of smaller distance continue
-      if (currentSum > target) {
-        r--;
-      } else {
-        l++;
-      }
+      if (currentSum > target) r--;
+      else l++;
     }
   }
 
   return currentSum;
 }
 
+//rahulbarik23
 // function threeSum(arr, target) {
 //   // arr.sort((a, b) => a - b);
 //   // let closestSum = Infinity;
